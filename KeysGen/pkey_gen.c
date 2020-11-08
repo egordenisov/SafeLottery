24f68a1406223ba8ef00d3f2c263c1b06856557f
Egor Zhukov <sazhuk2000@bk.ru>
2020-11-09 01:48:00 +0300

    Подредактировал pkey_gen.c, убрал ошибкu

--- KeysGen/pkey_gen.c
+++ KeysGen/pkey_gen.c
@@ -1,30 +1,48 @@
-//generate random numbers from Z_n^*
-int randZ_n(int *a,int *p, int *q){
-    a = rand()%(*p**q);
-    while (a == *p)||(a== *q)
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+int randZ_n (int p, int q); //generate random numbers from Z_n^*
+void pkey_gen (int p_, int q_); //generate Key
+
+int randZ_n (int p, int q)
+{
+    int output = 0;
+    output = rand () % (p * q);
+    while ((output == p) || (output == q))
     {
-        a = rand()%(*p**q)
+        output = rand () % (p * q);
     }        
-    return a
+    return output;
 }
-// хочу записать в файл
-void pkey_gen(int *p_, int *q_){
+
+void pkey_gen(int p_, int q_)
+{
     srand(time(NULL));
-    int i;
-    //define parameters
-    int n, p, q, m, a, b, beta;//parameters to private keys
-    p = 2**p_ + 1;
-    q = 2**q_ + 1;
+
+    //define parameters to private keys
+    int n = 0;
+    int p = 0, q = 0;
+    int m = 0, a = 0, b = 0, beta = 0;
+
+    p = 2 * p_ + 1;
+    q = 2 * q_ + 1;
     n = p * q;
     m = p_ * q_;
+
     //define a,b from Z_n^*
-    a = randZ_n(a, p, q);
-    b = randZ_n(b, p, q);
-    beta = randZ_n(beta, n, p, q);
+    a = randZ_n(p, q);
+    b = randZ_n(p, q);
+    beta = randZ_n(p, q);
+
     //define public key
-    PK[3] = {0, 0, 0};
-    PK[0] = pow(1+n, a)*pow(b, n)%(n*n);
+    long int PK[3] = {0, 0, 0};
+    PK[0] = (long int)(powl (1 + n, a) * powl (b, n)) % (n * n);
     PK[1] = n;
-    PK[2] = a*m*beta%n;
-    
+    PK[2] = (a * m * beta) % n;
+}
+
+int main ()
+{
+    return 0;
 }
